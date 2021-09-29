68ba0a595b73dccb2f79a563fa75053d62c9d495
wangying <747470666@qq.com>
2021-09-29 12:54:40 +0800

    mini-fix

--- src/components/article.py
+++ src/components/article.py
@@ -1,10 +1,20 @@
 from dash import html
 from dash import dcc
 
+style_article_img = [
+    {
+        'float': 'left',
+        'padding': '20px 20px 8px 0px',
+    },
+    {
+        'float': 'right',
+        'padding': '20px 0px 8px 20px',
+    }
+]
 
-def creat_article(img_src=[], text_content=[], first_position='LEFT'):
-    num_record = 0 if first_position == 'LEFT' else 1
-    position_list = ['left', 'right']
+
+def creat_article(img_src=[], text_content=[], first_position='left'):
+    num_record = 0 if first_position == 'left' else 1
     marry_len = min(len(img_src), len(text_content))
     article_list = []
     for i in range(marry_len):
@@ -13,7 +23,7 @@ def creat_article(img_src=[], text_content=[], first_position='LEFT'):
                 className='body_article_div',
                 children=[
                     html.Img(src=img_src[i], className='body_article_img',
-                             style={'float': position_list[num_record % 2]}),
+                             style=style_article_img[num_record % 2]),
                     html.P(text_content[i], className='body_article_p')
                 ]
             )
